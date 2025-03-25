8c881aead8ae7314173c7d38b622a787185e6687
Tim Nagorskikh <tim@waretronix.com>
2025-03-25 23:23:42 +0300

    v0.1.0 upload

---
+++ goose.go
@@ -0,0 +1,66 @@
+package goose
+
+import (
+	"context"
+	"log"
+	"strings"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"github.com/google/uuid"
+)
+
+
+func Upload(endpoint string, accessKeyID string, secretAccessKey string, bucketName string, filePath string, location string, objectName string) string {
+        ctx := context.Background()
+        useSSL := true
+
+        // Initialize minio client object.
+        minioClient, err := minio.New(endpoint, &minio.Options{
+                Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+                Secure: useSSL,
+        })
+        if err != nil {
+                log.Fatalln(err)
+        }
+
+        // Check if bucket exists
+        //if so, we'll upload to this bucket, if not - create new
+        if bucketName != "" {
+                err = minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: location})
+        if err != nil {
+                // Check to see if we already own this bucket (which happens if you run this twice)
+                exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+                if errBucketExists == nil && exists {
+                        log.Printf("We already own %s\n", bucketName)
+                } else {
+                        log.Fatalln(err)
+                }
+        } else {
+                log.Printf("Successfully created %s\n", bucketName)
+        }
+        } else {
+                log.Fatalln("Empty bucket name")
+        }
+        
+        // Upload the test file
+        if objectName == "" {
+                object := strings.Split(filePath, ".")
+				id := uuid.New()
+				objectName = id.String() + "." + object[len(object)-1]
+        }
+        contentType := "application/octet-stream"
+
+        // Upload the test file with FPutObject
+        info, err := minioClient.FPutObject(ctx, bucketName, objectName, filePath, minio.PutObjectOptions{ContentType: contentType})
+        if err != nil {
+                log.Fatalln(err)
+        }
+
+        log.Printf("Successfully uploaded %s of size %d\n", objectName, info.Size)
+
+		res := "https://" + endpoint + "/" + bucketName + "/" + objectName
+
+		log.Printf("Uploaded to %s", res)
+        return res
+}
